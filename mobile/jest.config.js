57f6c32d5269643bc32dd144cc0850278929c734
PhilippeFerreiraDeSousa <philippe@mentraglass.com>
2026-04-05 20:51:01 -0700

    Fix assets path

--- mobile/jest.config.js
+++ mobile/jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   preset: "jest-expo",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   moduleNameMapper: {
-    "^@assets/(.*)$": "<rootDir>/../assets/$1",
+    "^@assets/(.*)$": "<rootDir>/assets/$1",
   },
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg|core|typesafe-ts|uniwind)",

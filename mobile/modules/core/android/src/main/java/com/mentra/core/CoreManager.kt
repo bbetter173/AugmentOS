f5cbac88407db5227cb863e081bc748fcb80c26c
fossephate <6175367+fossephate@users.noreply.github.com>
2026-04-15 11:01:27 -0700

    fix

--- mobile/modules/core/android/src/main/java/com/mentra/core/CoreManager.kt
+++ mobile/modules/core/android/src/main/java/com/mentra/core/CoreManager.kt
@@ -974,7 +974,7 @@ class CoreManager {
             // sgc = FrameManager()
         }
         // update device model:
-        GlassesStore.shared.apply("glasses", "deviceModel", sgc?.type ?: "")
+        GlassesStore.apply("glasses", "deviceModel", sgc?.type ?: "")
     }
 
     fun restartTranscriber() {
